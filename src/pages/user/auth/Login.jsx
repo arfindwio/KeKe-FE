7333e7d71f24773b12bc25e339f7453d950cf802
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2024-05-01 19:42:57 +0700

    feat: add Login, Register, adn Otp Page

---
+++ src/pages/user/auth/Login.jsx
@@ -0,0 +1,185 @@
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import toast from "react-hot-toast";
+
+// Icons
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import { FcGoogle } from "react-icons/fc";
+
+// Redux Actions
+import { postLoginUserAction } from "../../../redux/action/users/UsersAction";
+
+// Image
+import Logo from "../../../assets/img/TravelesiaLogo.svg";
+
+// Helper
+import {
+  showSuccessToast,
+  showLoadingToast,
+  showErrorToast,
+} from "../../../helper/ToastHelper";
+
+export const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [inputLogin, setInputLogin] = useState({
+    emailOrPhoneNumber: "",
+    password: "",
+  });
+
+  const handleInputChange = (e, field) => {
+    setInputLogin((prevInputLogin) => ({
+      ...prevInputLogin,
+      [field]: e.target.value,
+    }));
+  };
+
+  const handleLogin = async (e) => {
+    if (e.key === "Enter" || e.type === "click") {
+      e.preventDefault();
+
+      const loadingToastId = showLoadingToast("Loading...");
+
+      const login = await dispatch(postLoginUserAction(inputLogin));
+
+      toast.dismiss(loadingToastId);
+
+      if (!login) showErrorToast("Login Failed");
+
+      if (login) {
+        showSuccessToast("Login Successful");
+        setTimeout(() => {
+          navigate("/");
+        }, 1000);
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="flex min-h-screen">
+        <div className="hidden min-h-screen items-center justify-center gap-2 bg-slate-700 bg-gradient-to-b from-15% via-65% md:flex md:w-1/2 md:flex-col">
+          <img
+            src={Logo}
+            alt="Travelesia Logo"
+            width={1}
+            height={1}
+            className="w-1/3"
+          />
+          <h1 className="text-neutral-5 font-sans md:text-5xl lg:text-6xl">
+            Travelesia
+          </h1>
+        </div>
+        <div className="flex w-full items-center px-[10%] md:w-1/2 md:px-10 lg:px-20 xl:px-[10%]">
+          <div className="flex w-full flex-col gap-4">
+            <form
+              className="flex w-full flex-col gap-4"
+              onKeyDown={handleLogin}
+            >
+              <h5 className="mb-2 text-2xl font-semibold">Login</h5>
+              <div className="flex w-full flex-col">
+                <label htmlFor="emailOrPhoneNumber">Email / Phone Number</label>
+                <input
+                  type="text"
+                  id="emailOrPhoneNumber"
+                  className="border-1 rounded-2xl border px-4 py-3 outline-none"
+                  placeholder="Email or Phone Number"
+                  value={inputLogin.emailOrPhoneNumber}
+                  onChange={(e) => {
+                    handleInputChange(e, "emailOrPhoneNumber");
+                  }}
+                />
+              </div>
+              <div className="flex w-full flex-col">
+                <div className="flex justify-between">
+                  <label htmlFor="password">Password</label>
+                  <Link
+                    href="/forget-password"
+                    className="text-neutral-1 hover:opacity-70"
+                  >
+                    Forget Password
+                  </Link>
+                </div>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    className="border-1 w-full rounded-2xl border px-4 py-3 pr-14 outline-none"
+                    placeholder="Masukkan password"
+                    value={inputLogin.password}
+                    onChange={(e) => {
+                      handleInputChange(e, "password");
+                    }}
+                    autoComplete="off"
+                  />
+                  {showPassword ? (
+                    <FiEye
+                      size={27}
+                      className="absolute right-4 top-3 w-8 cursor-pointer text-slate-400"
+                      onClick={() => {
+                        setShowPassword(!showPassword);
+                      }}
+                    />
+                  ) : (
+                    <FiEyeOff
+                      size={27}
+                      className="absolute right-4 top-3 w-8 cursor-pointer text-slate-400"
+                      onClick={() => {
+                        setShowPassword(!showPassword);
+                      }}
+                    />
+                  )}
+                </div>
+              </div>
+              <button
+                className="text-neutral-5 bg-neutral-1 mt-3 w-full rounded-2xl py-4 text-base font-medium hover:bg-opacity-80"
+                onClick={handleLogin}
+              >
+                Login
+              </button>
+            </form>
+            <div className="mx-auto flex w-full items-center justify-between">
+              <span className="border-neutral-4 w-[45%] border-t-2"></span>
+              <p className="text-neutral-4 text-lg font-medium italic">OR</p>
+              <span className="border-neutral-4 w-[45%] border-t-2"></span>
+            </div>
+            <button
+              className="bg-neutral-5 hover:bg-neutral-1 border-neutral-1 group flex w-full items-center justify-center gap-3 rounded-2xl border-2  py-3 text-base font-medium"
+              onClick={() =>
+                (window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/google`)
+              }
+            >
+              <FcGoogle size={30} />
+              <p className="text-neutral-1 group-hover:text-neutral-5">
+                Login With Google
+              </p>
+            </button>
+            <div className="mt-1">
+              <p className="text-center text-sm">
+                Don't have an account?
+                <Link
+                  to={"/register"}
+                  className="text-neu1border-neutral-1 ms-2 font-bold hover:opacity-60"
+                >
+                  Register here
+                </Link>
+              </p>
+              <p className="text-center text-sm">
+                Account not verified?
+                <Link
+                  to={"/verify-account"}
+                  className="text-neu1border-neutral-1 ms-2 font-bold hover:opacity-60"
+                >
+                  Verify it here
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/pages/user/auth/Otp.jsx
@@ -0,0 +1,168 @@
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import toast from "react-hot-toast";
+
+// Redux Action
+import {
+  putVerifyOtpAction,
+  putResendOtp,
+} from "../../../redux/action/users/UsersAction";
+
+// Helper
+import {
+  showSuccessToast,
+  showLoadingToast,
+  showErrorToast,
+} from "../../../helper/ToastHelper";
+
+// Components
+import OtpInput from "../../../assets/components/auth/OtpInput";
+
+// icons
+import { IoArrowBack } from "react-icons/io5";
+
+// Image
+import Logo from "../../../assets/img/TravelesiaLogo.svg";
+
+export const Otp = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const [valueEmail, setValueEmail] = useState(null);
+  const [finalOtp, setFinalOtp] = useState("");
+  const [countdown, setCountdown] = useState(60);
+
+  useEffect(() => {
+    if (!location.state) return navigate(`/register`);
+    if (!valueEmail) setValueEmail(location.state.email);
+  }, [valueEmail, setValueEmail, location, navigate]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prevCountdown) => Math.max(prevCountdown - 1, 0));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const maskEmail = (email) => {
+    const prefix = email.substring(0, email.indexOf("@"));
+    const maskedPrefix =
+      prefix.length > 1
+        ? prefix.charAt(0) + "*".repeat(prefix.length - 1)
+        : prefix;
+    const maskedEmail = maskedPrefix + email.substring(email.indexOf("@"));
+
+    return maskedEmail;
+  };
+
+  const handleVerifyOtp = async (e) => {
+    if (e.key === "Enter" || e.type === "click") {
+      e.preventDefault();
+
+      const loadingToastId = showLoadingToast("Loading...");
+      let verifyOtp = false;
+
+      if (valueEmail && finalOtp)
+        verifyOtp = await dispatch(
+          putVerifyOtpAction({
+            email: valueEmail,
+            otp: finalOtp,
+          }),
+        );
+
+      toast.dismiss(loadingToastId);
+
+      if (!verifyOtp) showErrorToast("Verify OTP Failed");
+      if (verifyOtp) {
+        showSuccessToast("verify OTP successful");
+        setTimeout(() => {
+          navigate("/login");
+        }, 1000);
+      }
+    }
+  };
+
+  const handleResendOtp = async (e) => {
+    if (e.type === "click") {
+      e.preventDefault();
+
+      const loadingToastId = showLoadingToast("Loading...");
+      let resendOtp = false;
+
+      if (valueEmail)
+        resendOtp = await dispatch(putResendOtp({ email: valueEmail }));
+
+      toast.dismiss(loadingToastId);
+
+      if (!resendOtp) showErrorToast("Resend OTP Failed");
+      if (resendOtp) {
+        showSuccessToast("Resend OTP successful");
+        setCountdown(60);
+      }
+    }
+  };
+
+  const handleOtpChange = (otp) => {
+    setFinalOtp(otp);
+  };
+
+  return (
+    <div className="flex min-h-screen">
+      <div className="hidden min-h-screen items-center justify-center gap-2 bg-slate-700 md:flex md:w-1/2 md:flex-col">
+        <img
+          src={Logo}
+          alt="Travelesia Logo"
+          width={1}
+          height={1}
+          className="w-1/3"
+        />
+        <h1 className="text-neutral-5 font-sans md:text-5xl lg:text-6xl">
+          Travelesia
+        </h1>
+      </div>
+      <div className="flex w-full items-center px-[10%] md:w-1/2 md:px-10 lg:px-20 xl:px-[10%]">
+        <form
+          className="flex w-full flex-col gap-2"
+          onKeyDown={handleVerifyOtp}
+        >
+          <button
+            className="relative flex w-fit items-center"
+            onClick={() => navigate(window.history.back())}
+          >
+            <IoArrowBack size={25} className="left-0 top-2" />
+            <p className="ms-2 text-lg">Back</p>
+          </button>
+          <h1 className="mb-4 text-2xl font-bold">Input OTP</h1>
+          <div className="flex flex-col gap-4 text-center">
+            <p className="text-sm">
+              Type the 6 digit code sent to{" "}
+              <span className="font-bold">
+                {valueEmail ? maskEmail(valueEmail) : ""}
+              </span>
+            </p>
+            <OtpInput onOtpChange={handleOtpChange} />
+            {countdown > 0 ? (
+              <p className="text-sm">Resend OTP in {countdown} seconds</p>
+            ) : (
+              <button
+                className="text-neutral-3 hover:text-neutral-2 mx-auto w-fit text-base font-semibold"
+                onClick={handleResendOtp}
+              >
+                Resend OTP
+              </button>
+            )}
+          </div>
+          <button
+            className="bg-neutral-1 text-neutral-5 mt-10 w-full rounded-2xl py-2 hover:bg-opacity-80"
+            onClick={handleVerifyOtp}
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
